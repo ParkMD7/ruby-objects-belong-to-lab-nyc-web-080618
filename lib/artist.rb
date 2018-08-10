47f28bb16b389ebf94e16ee3fa4890d7448df97f
ParkMD7 <parkermdinsmore@gmail.com>
2018-08-09 23:48:53 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class Artist
+
+attr_accessor :name
+
+  def initialize
+    @name = name
+  end
+
+
+
+end #End Artist Class
+
+#binding.pry
---
+++ lib/post.rb
@@ -0,0 +1,12 @@
+require "pry"
+
+class Post
+
+attr_accessor :title, :author
+
+def initialize
+  @title = title
+end
+
+
+end #End of Post Class
